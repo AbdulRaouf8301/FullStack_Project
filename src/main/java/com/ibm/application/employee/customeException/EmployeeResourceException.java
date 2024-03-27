a510268328cd9d4214484de31ddb2b0a07a216af
AbdulRaouf8301 <151602494+AbdulRaouf8301@users.noreply.github.com>
2024-03-27 16:14:25 +0530

    Add files via upload

---
+++ src/main/java/com/ibm/application/employee/customeException/EmployeeResourceException.java
@@ -0,0 +1,9 @@
+package com.ibm.application.employee.customeException;
+
+public class EmployeeResourceException extends Exception {
+
+	public  EmployeeResourceException(String msg)
+	{
+		super(msg);
+	}
+}
