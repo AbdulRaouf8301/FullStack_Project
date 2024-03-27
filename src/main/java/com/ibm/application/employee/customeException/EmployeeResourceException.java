8a89ccb6dc3c1660ceb57fa1afd89b3c42b34343
AbdulRaouf8301 <151602494+AbdulRaouf8301@users.noreply.github.com>
2024-03-27 15:48:53 +0530

    Delete src directory

--- src/main/java/com/ibm/application/employee/customeException/EmployeeResourceException.java
+++
@@ -1,9 +0,0 @@
-package com.ibm.application.employee.customeException;
-
-public class EmployeeResourceException extends Exception {
-
-	public  EmployeeResourceException(String msg)
-	{
-		super(msg);
-	}
-}
--- src/main/java/com/ibm/application/employee/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ibm.application.employee.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ibm.application.employee.model.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee,Long>{
-
-}
